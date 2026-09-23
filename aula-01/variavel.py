b016641143fc13f746b1bfbd91db7e3abb8ba40a
Hector Juan <ueqtor@gmail.com>
2026-09-22 23:20:15 -0300

    aula 1: subindo comentarios

--- aula-01/variavel.py
+++ aula-01/variavel.py
@@ -4,8 +4,8 @@ estado = "Sao Paulo"  # String (str)
 tem_cachorro = True  # Boolean (bool)
 altura = 1.85  # Float (float)
 
-print(type(nome))
-print(type(idade))
-print(type(estado))
-print(type(tem_cachorro))
-print(type(altura))
+print(type(nome))  # retorna o tipo de variavel string
+print(type(idade))  # retorna o tipo de variavel integer
+print(type(estado))  # retorna o tipo de variavel string
+print(type(tem_cachorro))  # retorna o tipo de variavel boolean
+print(type(altura))  # retorna o tipo de variavel float
